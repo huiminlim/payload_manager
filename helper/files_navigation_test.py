24a3de9a2bc89c055259ace18edfa125ac586a56
huiminlim <e0036379@u.nus.edu>
2021-02-14 17:11:30 +0800

    Retrieve filepath list code

--- helper/files_navigation_test.py
+++ helper/files_navigation_test.py
@@ -21,8 +21,28 @@ start = process_timestamp('2021-01-19_17:45:40')
 end = process_timestamp('2021-01-19_17:48:00')
 
 
-for t in os.listdir('/home/pi/Desktop/Mission'):
-    if start < process_timestamp(t) and process_timestamp(t) < end:
-        #print(os.listdir('/home/pi/Desktop/Mission' + '/' + t))
-        for file in os.listdir('/home/pi/Desktop/Mission' + '/' + t):
-            print('/home/pi/Desktop/Mission' + '/' + t + '/' + file)
+
+
+# for t in os.listdir('/home/pi/Desktop/Mission'):
+#     if start < process_timestamp(t) and process_timestamp(t) < end:
+#         #print(os.listdir('/home/pi/Desktop/Mission' + '/' + t))
+#         for file in os.listdir('/home/pi/Desktop/Mission' + '/' + t):
+#             print('/home/pi/Desktop/Mission' + '/' + t + '/' + file)
+
+
+def process_downlink_filepaths(start_timestamp, end_timestamp):
+    MISSION_ROOT_FILEPATH = '/home/pi/Desktop/Mission'
+
+    list_filepaths = []
+
+    for timestamp in os.listdir('/home/pi/Desktop/Mission'):
+        processed_timestamp = process_timestamp(timestamp)
+        if start_timestamp < processed_timestamp and processed_timestamp < end_timestamp:
+            for file in os.listdir('/home/pi/Desktop/Mission' + '/' + timestamp):
+                list_filepaths.append(
+                    '/home/pi/Desktop/Mission' + '/' + timestamp + '/' + file)
+
+    return list_filepaths
+
+
+print(process_downlink_filepaths(start, end))
