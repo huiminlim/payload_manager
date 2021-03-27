5609449678ae10b4387ed795cc82d17add0fe70c
huiminlim <e0036379@u.nus.edu>
2021-03-27 17:50:28 +0800

    debug mysterious prints

--- payload_manager.py
+++ payload_manager.py
@@ -38,51 +38,54 @@ def main():
                 print("Read data from serial input")
                 data_read = ser_cmd_input.readline().decode("utf-8").replace("\r\n", "")
 
-            list_data_read = data_read.split(" ")
+            if data_read:
+                list_data_read = data_read.split(" ")
 
-            print(list_data_read)
+                print(list_data_read)
 
-            cmd = list_data_read[0]
+                cmd = list_data_read[0]
 
-            # Technically redundant since merged mission and downlink command
-            if cmd == 'md':
+                # Technically redundant since merged mission and downlink command
+                if cmd == 'md':
 
-                mission_list = list_data_read[1:4]
-                downlink_start_ts = list_data_read[4]
+                    mission_list = list_data_read[1:4]
+                    downlink_start_ts = list_data_read[4]
 
-                # Mission
-                datetime_obj, num, list_ts_image = process_mission_command(
-                    mission_list)
+                    # Mission
+                    datetime_obj, num, list_ts_image = process_mission_command(
+                        mission_list)
 
-                # Create folder path part applicable for mission only
-                # Create Folder for mission
-                storage_path = MISSION_ROOT_FILEPATH
+                    # Create folder path part applicable for mission only
+                    # Create Folder for mission
+                    storage_path = MISSION_ROOT_FILEPATH
 
-                mission_folder_path = storage_path + '/' + \
-                    datetime_obj.strftime("%Y-%m-%d_%H:%M:%S")
-                os.mkdir(mission_folder_path)
-                print("Mission directory created: %s" % mission_folder_path)
+                    mission_folder_path = storage_path + '/' + \
+                        datetime_obj.strftime("%Y-%m-%d_%H:%M:%S")
+                    os.mkdir(mission_folder_path)
+                    print("Mission directory created: %s" %
+                          mission_folder_path)
 
-                count = 0
-                for ts in list_ts_image:
-                    count = count + 1
-                    scheduler.add_job(mission_cmd, run_date=ts, args=[
-                        camera, mission_folder_path, ts, count, num])
+                    count = 0
+                    for ts in list_ts_image:
+                        count = count + 1
+                        scheduler.add_job(mission_cmd, run_date=ts, args=[
+                            camera, mission_folder_path, ts, count, num])
 
-                # Downlink
-                # Process all 3 timestamps
-                timestamp_start_downlink = process_timestamp(downlink_start_ts)
-                # timestamp_query_start = process_timestamp(downlink_list[2])
-                # timestamp_query_end = process_timestamp(downlink_list[3])
+                    # Downlink
+                    # Process all 3 timestamps
+                    timestamp_start_downlink = process_timestamp(
+                        downlink_start_ts)
+                    # timestamp_query_start = process_timestamp(downlink_list[2])
+                    # timestamp_query_end = process_timestamp(downlink_list[3])
 
-                # Obtain list of filepaths to images to downlink
-                # filepath_list = process_downlink_filepaths(
-                #     timestamp_query_start, timestamp_query_end)
+                    # Obtain list of filepaths to images to downlink
+                    # filepath_list = process_downlink_filepaths(
+                    #     timestamp_query_start, timestamp_query_end)
 
-                scheduler.add_job(download_cmd, next_run_time=timestamp_start_downlink, args=[
-                    ser_downlink, mission_folder_path])
+                    scheduler.add_job(download_cmd, next_run_time=timestamp_start_downlink, args=[
+                        ser_downlink, mission_folder_path])
 
-                no_exception = True
+                    no_exception = True
 
         except KeyboardInterrupt:
             print("End, exiting")
