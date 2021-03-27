2a34ebdd5a6063aeb5e83f1d1869c19b99780e3a
huiminlim <e0036379@u.nus.edu>
2021-03-27 17:21:02 +0800

    debug

--- payload_manager.py
+++ payload_manager.py
@@ -95,7 +95,7 @@ def main():
             print("Error -- unicode decode error")
             print("Request cmd again...")
 
-            ser_cmd_input.write("bcc\r\n")
+            ser_cmd_input.write(b"bcc\r\n")
             data_read = ser_cmd_input.readline().decode("utf-8").replace("\r\n", "")
             no_exception = False
 
